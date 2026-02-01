bee5d3ca3a73dafb768fc906fb6f4568b79b75f8
Roberto Decourt <roberto.decourt@gmail.com>
2026-02-01 18:06:25 -0300

    fix webhook verify token env var

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ app.get("/webhook", (req, res) => {
   const token = req.query["hub.verify_token"];
   const challenge = req.query["hub.challenge"];
 
-  if (mode === "subscribe" && token === process.env.Cebola_#197a!hfdcjufpg$w) {
+  if (mode === "subscribe" && token === process.env.WA_VERIFY_TOKEN) {
     return res.status(200).send(challenge);
   }
   return res.sendStatus(403);
